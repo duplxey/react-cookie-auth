570774a8fc0c825d8b7e0a77f1f09b8d0ee1fa4c
duplxey <nick.tomazic@gmail.com>
2020-11-08 17:47:34 +0100

    Added LoginPage logic.

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,44 @@
 import React from "react";
 
-const LoginPage = () => {
-  return (
-    <p>Login</p>
-  )
-};
+class LoginPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+
+    const state = {...this.state};
+    state[event.target.name] = event.target.value;
+    this.setState(state);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // TODO: send the request to the API
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="username">Username: </label>
+        <input type="text" name="username" value={this.state.username} onChange={this.handleChange} /><br />
+        <label htmlFor="password">Password: </label>
+        <input type="text" name="password" value={this.state.password} onChange={this.handleChange} /><br />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 export default LoginPage;
